d3651a005a99584fe7d1e192a389b6a352669e1d
neegorolewa <cullofdute19762005@gmail.com>
2025-04-09 04:35:49 +0300

    complete dictionary

---
+++ Dictionary/Dictionary/Dictionary.cs
@@ -0,0 +1,78 @@
+﻿namespace Dictionary;
+
+public class Dictionary
+{
+    private const string DictFilePath = "C:\\Users\\Eugene\\TL_practice\\Dictionary\\Dictionary\\dict.txt";
+    private const char TranslateSeparator = ':';
+    private const string TranslationNotFound = "";
+
+    private readonly Dictionary<string, string> _dictionary;
+    public Dictionary()
+    {
+        _dictionary = new Dictionary<string, string>();
+
+        LoadDictionaryFromFile();
+    }
+
+    private void LoadDictionaryFromFile()
+    {
+        if ( !File.Exists( DictFilePath ) )
+            return;
+
+        StreamReader file = new StreamReader( DictFilePath );
+
+        while ( !file.EndOfStream )
+        {
+            string line = file.ReadLine();
+            if ( string.IsNullOrEmpty( line ) )
+                continue;
+
+            string[] parts = line.Split( TranslateSeparator );
+            if ( parts.Length != 2 )
+                continue;
+
+            string word = parts[ 0 ].Trim();
+            string translate = parts[ 1 ];
+
+            _dictionary.Add( word, translate );
+        }
+
+        file.Close();
+    }
+
+    public string GetTranslation( string word )
+    {
+        if ( _dictionary.TryGetValue( word, out string translation ) )
+        {
+            return translation;
+        }
+
+        return TranslationNotFound;
+    }
+
+    public void AddTranslation( string word, string translate )
+    {
+        if ( _dictionary.ContainsKey( word ) )
+        {
+            Console.WriteLine( "Перевод с таким словом уже существует" );
+            return;
+        }
+
+        _dictionary.Add( word, translate );
+        return;
+    }
+
+    public void SaveDictionaryToFile()
+    {
+        if ( _dictionary.Count == 0 ) return;
+
+        StreamWriter file = new StreamWriter( DictFilePath );
+
+        foreach ( var pair in _dictionary )
+        {
+            file.WriteLine( $"{pair.Key}{TranslateSeparator}{pair.Value}" );
+        }
+
+        file.Close();
+    }
+}
--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -1,2 +1,124 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Dictionary;
+
+public class Program
+{
+    const string GetTranslationCommand = "1";
+    const string AddTranslationCommand = "2";
+    const string ClearScreenCommand = "3";
+    const string ExitCommand = "4";
+
+    private static void Main( string[] args )
+    {
+        Dictionary dictionary = new Dictionary();
+        RunProgramm( dictionary );
+    }
+
+    public static void PrintMenu()
+    {
+        Console.WriteLine( "Выберите действие: \n" +
+           $"({GetTranslationCommand}) Перевести слово\n" +
+           $"({AddTranslationCommand}) Добавить перевод\n" +
+           $"({ClearScreenCommand}) Убрать лишнее с экрана\n" +
+           $"({ExitCommand}) Выйти" );
+    }
+
+    public static void RunProgramm( Dictionary dict )
+    {
+        while ( true )
+        {
+
+            PrintMenu();
+            Console.Write( "Введите команду: " );
+
+            var command = Console.ReadLine();
+
+            switch ( command )
+            {
+                case GetTranslationCommand:
+                    GetTranslationFromDictionary( dict );
+                    break;
+
+                case AddTranslationCommand:
+                    AddTranslationToDictionary( dict );
+                    break;
+
+                case ClearScreenCommand:
+                    Console.Clear();
+                    break;
+
+                case ExitCommand:
+                    dict.SaveDictionaryToFile();
+                    return;
+
+                default:
+                    Console.WriteLine( "Неверная команда!" );
+                    break;
+            }
+        }
+    }
+
+    public static void GetTranslationFromDictionary( Dictionary dict )
+    {
+        Console.Write( "Введите слово для перевода: " );
+        string inputWord = GetStringValue();
+
+        string translate = dict.GetTranslation( inputWord );
+
+        if ( translate.Length > 0 )
+        {
+            Console.WriteLine( $"{inputWord} - {translate}" );
+        }
+        else
+        {
+            Console.Write( "Перевод остутствует. Хотите добавить перевод? (у/n): " );
+            while ( true )
+            {
+                string answer = Console.ReadLine();
+                if ( answer == "y" )
+                {
+                    AddTranslationToDictionary( dict, inputWord );
+                    return;
+                }
+                else if ( answer == "n" )
+                {
+                    return;
+                }
+                else
+                {
+                    Console.Write( "Введите верную команду (y/n): " );
+                }
+            }
+        }
+    }
+
+    public static void AddTranslationToDictionary( Dictionary dict, string? word = null )
+    {
+        if ( word == null )
+        {
+            Console.Write( "Введите слово для перевода: " );
+            word = GetStringValue();
+        }
+
+        Console.Write( $"Введите перевод для '{word}': " );
+        string inputTranslate = GetStringValue();
+        dict.AddTranslation( word, inputTranslate );
+
+        return;
+    }
+
+    public static string GetStringValue()
+    {
+        while ( true )
+        {
+            string value = Console.ReadLine();
+
+            if ( string.IsNullOrEmpty( value ) )
+            {
+                Console.Write( "Пожалуйста, введите значение: " );
+                continue;
+            }
+
+            return value;
+        }
+    }
+}
